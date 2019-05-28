407eb5e34c321e1cbccb35e98764f6c00d5478fa
IkramMissaoui <missaoui.ikram726@gmail.com>
2019-05-28 17:32:30 +0200

    Updates

---
+++ precache-manifest.d4313052127407e01e2f071bdaa81ef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d058018496c4cc81e8450305c4c06e74",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "00380f580189191aca65",
+    "url": "/movieapp/static/css/main.872e0f88.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/movieapp/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "00380f580189191aca65",
+    "url": "/movieapp/static/js/main.26983c97.chunk.js"
+  },
+  {
+    "revision": "4e19d316896026f2633d",
+    "url": "/movieapp/static/js/runtime~main.839b6e54.js"
+  }
+]);
